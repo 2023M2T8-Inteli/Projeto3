77534f8708cef5dfc8f05e204fc52341fc8976d4
pedrocruz2 <pedro.cruz@sou.inteli.edu.br>
2023-05-12 20:43:17 -0300

    ENDPOINTS INSERT FUBNCIOINANDO

--- src/Backend/endpoints.js
+++ src/Backend/endpoints.js
@@ -9,7 +9,7 @@ if (err){
 } else{
 	console.log('Connected to the Database. ');
 }});
-  
+   
 app.use(express.json());
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
@@ -29,12 +29,25 @@ app.listen(port, () => {
 			}
 			res.json(rows);
 			});
+			close(db);
 	  });
-	  app.get('/vagao/tipoG', (req, res) => {
-		db.all('SELECT * FROM vagao WHERE tipo_vagao = "g"', [], (err, rows) => {
+	  app.get('/vagao/tipoF', (req, res) => {
+		db.all('SELECT * FROM vagao WHERE tipo_vagao = "f"', [], (err, rows) => { 
 		  if (err) {
 				console.error(err.message);
 			}
 			res.json(rows);
+			
 			});
+	  });
+	  app.post('/inserirVagao', (req, res) => { 
+		const xD = 'INSERT INTO vagao (placa, tipo_vagao) VALUES (?, ?)';
+		db.run(xD, [req.body.placa, req.body.tipo_vagao], (err) => {
+		  if (err) {
+			console.error(err.message);
+			res.status(500).json({ error: 'Failed to insert row' });
+		  } else {
+			res.status(201).json({ message: 'Row inserted successfully' });
+		  }
+		});
 	  });
