8cd0d5e7ef292a671e39e811bb0a544430cda4a5
pedrocruz2 <pedro.cruz@sou.inteli.edu.br>
2023-06-11 22:39:34 -0300

    comentarios2

--- src/Frontend/Main/script.js
+++ src/Frontend/Main/script.js
@@ -1,4 +1,4 @@
-function acharChoquePlaca(){
+function acharChoquePlaca(){ //Aqui reconheço um elemento no HTML para realizar a consulta de choque por placa do vagão
     console.log('Acharchoque1')
     var user = {
         placa: document.getElementById('placa').value,
@@ -14,19 +14,22 @@ function acharChoquePlaca(){
         resultElement.textContent = JSON.stringify(data); // Atualiza o conteúdo do elemento com a resposta recebida
     })
     .catch(err => console.log(err));
-}
-
-function send2() { 
-    console.log('Botao2 funcionou')
+} 
+function acharChoqueTipo(){ //Aqui reconheço um elemento no HTML para realizar a consulta de choque por tipo do vagão
+    console.log('Acharchoque2')
     var user = {
-        email: document.getElementById('email').value,
-        password: document.getElementById('password').value,
+        tipo_vagao: document.getElementById('tipo_vagao').value,
     }
-    fetch('http://127.0.0.1:3200/cadastrar', {
+    fetch('http://localhost:9696/choqueVagaoTipo', {
         method: 'POST',
         body: JSON.stringify(user),
         headers: {"Content-type": "application/json"}
     })
     .then(response => response.json())
-    .catch(err => console.log(err))
+    .then(data => {
+        var resultElement = document.getElementById('result');
+        resultElement.textContent = JSON.stringify(data); // Atualiza o conteúdo do elemento com a resposta recebida
+    })
+     .catch(err => console.log(err));
 }
+
